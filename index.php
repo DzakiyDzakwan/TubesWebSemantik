d92cc577ef485fda771de7e0950844052d7a6d8d
Dzakiy Dzakwan <dzakcart@gmail.com>
2022-12-14 08:51:27 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,357 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Elon Musk</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,500,500i,600,600i,700,700i|Playfair+Display:400,400i,500,500i,600,600i,700,700i,900,900i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- LeafletJs -->
+  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+    integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI=" crossorigin="" />
+  <!-- Make sure you put this AFTER Leaflet's CSS -->
+  <!-- Make sure you put this AFTER Leaflet's CSS -->
+  <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+    integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM=" crossorigin=""></script>
+
+  <style>
+    #map {
+      height: 200px;
+    }
+  </style>
+</head>
+
+<body>
+  <!-- ======= Hero Section ======= -->
+  <div id="hero" class="home">
+
+    <div class="container">
+      <div class="hero-content">
+        <h1>I'm <span class="typed" data-typed-items="Elon Musk"></span></h1>
+        <p>Elon Reeve Musk</p>
+      </div>
+    </div>
+  </div><!-- End Hero -->
+
+  <main id="main">
+
+    <!-- ======= About Section ======= -->
+    <div class="paddsection">
+      <div class="container">
+        <div class="row justify-content-between">
+
+          <div class="col-lg-12">
+            <div class="about-descr">
+              <p> Elon Reeve Musk FRS (/'i:lɒn/ EE-lon; born June 28, 1971) is a business magnate and investor. He is
+                the founder, CEO, and Chief Engineer at SpaceX;
+                angel investor, CEO, and product architect of Tesla, Inc.; founder of The Boring Company; and co-founder
+                of Neuralink and OpenAI. With an estimated net worth
+                of around $254.6 billion as of September 26, 2022, Musk is the wealthiest person in the world according
+                to both the Bloomberg Billionaires Index and Forbes'
+                real-time billionaires list. Musk was born in Pretoria, South Africa, where he grew up. He briefly
+                attended the University of Pretoria before moving to Canada
+                at age 17, acquiring citizenship through his Canadian-born mother. Two years later, he matriculated at
+                Queen's University and transferred to the University of
+                Pennsylvania, where he received bachelor's degrees in economics and physics. He moved to California in
+                1995 to attend Stanford University but decided instead
+                to pursue a business career, co-founding the web software company Zip2 with his brother Kimbal. The
+                startup was acquired by Compaq for $307 million in 1999.
+                The same year, Musk co-founded online bank X.com, which merged with Confinity in 2000 to form PayPal.
+                eBay bought PayPal in 2002, for $1.5 billion. In 2002,
+                Musk founded SpaceX, an aerospace manufacturer and space transport services company, of which he serves
+                as CEO and Chief Engineer. In 2004, he was an early
+                investor in electric vehicle manufacturer Tesla Motors, Inc. (now Tesla, Inc.). He became its chairman
+                and product architect, eventually assuming the position
+                of CEO in 2008. In 2006, he helped create SolarCity, a solar energy company that was later acquired by
+                Tesla and became Tesla Energy. In 2015, he co-founded
+                OpenAI, a nonprofit research company promoting friendly artificial intelligence (AI). In 2016, he
+                co-founded Neuralink, a neurotechnology company focused
+                on developing brain-computer interfaces, and founded The Boring Company, a tunnel construction company.
+                Musk has proposed a hyperloop high-speed vactrain
+                transportation system and is the president of the Musk Foundation, which donates to scientific research
+                and education. Musk has been criticized for making
+                unscientific and controversial statements, such as spreading misinformation about the COVID-19 pandemic
+                and promoting contentious perspectives regarding
+                politics and various technologies, especially on Twitter. In 2018, the U.S. Securities and Exchange
+                Commission (SEC) sued Musk for tweeting that he had
+                secured funding for a private takeover of Tesla, which the SEC characterized as false and misleading. He
+                settled with the SEC but did not admit guilt,
+                and he temporarily stepped down from his Tesla chairmanship. In 2019, he won a defamation case brought
+                against him by a British caver who had advised
+                in the Tham Luang cave rescue. In 2022, Musk agreed to purchase Twitter for $44 billion but later
+                submitted a termination letter of the deal, leading
+                to lawsuits between both parties.</p>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End About Section -->
+    <div id="about" class="paddsection">
+      <div class="container">
+        <div class="row justify-content-between">
+
+          <div class="col-lg-4 ">
+            <div class="div-img-bg">
+              <div class="about-img">
+                <img src="http://commons.wikimedia.org/wiki/Special:FilePath/Elon_Musk_Royal_Society_(crop2).jpg"
+                  class="img-responsive" alt="me">
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-7">
+            <div class="about-descr">
+              <h2>Elon Musk</h2>
+              <ul>
+                <li>Birth Name : Elon Reeve Musk</li>
+                <li>BirthOn : Transval Province, 1971-01-01</li>
+                <li>Education : University of Pennsylvania, Bachelor of Science</li>
+                <li>
+                  Relative :
+                  <ul>
+                    <li>Kimbal Musk</li>
+                    <li>Tosca Musk</li>
+                    <li>Lyndon Rive</li>
+                  </ul>
+                </li>
+                <li>
+                  Spouse :
+                  <ul>
+                    <li>Talulah Riley</li>
+                    <li>Justine Musk</li>
+                  </ul>
+                </li>
+                <li>
+                  Family :
+                  <ul>
+                    <li>Father : Errol Musk</li>
+                    <li>Mother : Maye Musk</li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- <div id="universitas" class="paddsection">
+      <div class="container">
+        <div class="row justify-content-between">
+
+          <div class="col-lg-4 ">
+            <div class="div-img-bg">
+              <div class="about-img">
+                <img src="https://th.bing.com/th/id/OIP.h_4LljOQs3XpH2LptvL-zgHaE7?pid=ImgDet&rs=1"
+                  class="img-responsive" alt="me">
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-7">
+            <div class="about-descr">
+              <li>University_of_Pennsylvania</li>
+              <li>Bachelor_of_Science</li>
+
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div> -->
+    <!-- End About Section -->
+
+    <div id="map" class="paddsection">
+    </div>
+
+    <!-- ======= Services Section ======= -->
+    <div id="Awards">
+      <div class="container">
+        <h1 class="text-center my-5">Awards</h1>
+        <div class="services-slider swiper" data-aos="fade-up" data-aos-delay="100">
+          <div class="swiper-wrapper">
+
+            <div class="swiper-slide">
+              <div class="services-block">
+                <i class="bi bi-trophy"></i>
+                <span>IEEE_Honorary_Membership</span>
+              </div>
+            </div><!-- End testimonial item -->
+            <div class="swiper-slide">
+              <div class="services-block">
+                <i class="bi bi-trophy"></i>
+                <span>IEEE_Honorary_Membership</span>
+              </div>
+            </div><!-- End testimonial item -->
+            <div class="swiper-slide">
+              <div class="services-block">
+                <i class="bi bi-trophy"></i>
+                <span>IEEE_Honorary_Membership</span>
+              </div>
+            </div><!-- End testimonial item -->
+            <div class="swiper-slide">
+              <div class="services-block">
+                <i class="bi bi-trophy"></i>
+                <span>IEEE_Honorary_Membership</span>
+              </div>
+            </div><!-- End testimonial item -->
+            <div class="swiper-slide">
+              <div class="services-block">
+                <i class="bi bi-trophy"></i>
+                <span>IEEE_Honorary_Membership</span>
+              </div>
+            </div><!-- End testimonial item -->
+            <div class="swiper-slide">
+              <div class="services-block">
+                <i class="bi bi-trophy"></i>
+                <span>IEEE_Honorary_Membership</span>
+              </div>
+            </div><!-- End testimonial item -->
+
+          </div>
+          <div class="swiper-pagination"></div>
+        </div>
+
+      </div>
+
+    </div><!-- End Services Section -->
+    <!-- ======= Journal Section ======= --><!---->
+    <div id="Saudara" class="text-left paddsection">
+
+      <div class="container">
+        <div class="section-title text-center">
+          <h1>Relative</h1>
+        </div>
+      </div>
+
+      <div class="container">
+        <div class="journal-block">
+          <div class="row">
+
+            <div class="col-lg-4 col-md-6">
+              <div class="journal-info">
+                <a href="blog-single.html">
+                  <img
+                    src="http://commons.wikimedia.org/wiki/Special:FilePath/KIMBAL_MUSK_OFFICIAL_HEADSHOT.jpg?width=300"
+                    class="img-responsive" alt="img">
+                  </a>
+
+                <div class="journal-txt">
+                  <h4><a href="blog-single.html">Kimbal Musk</a></h4>
+                </div>
+
+              </div>
+            </div>
+
+            <div class="col-lg-4 col-md-6">
+              <div class="journal-info">
+
+                <a href="blog-single.html"><img
+                    src="http://commons.wikimedia.org/wiki/Special:FilePath/Tosca-Musk_Founder-CEO_Passionflix_March_1_2019.jpg?width=300"
+                    class="img-responsive" alt="img"></a>
+
+                <div class="journal-txt">
+
+                  <h4><a href="blog-single.html">Tosca Musk</a></h4>
+
+
+                </div>
+
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6">
+              <div class="journal-info">
+
+                <a href="blog-single.html"><img
+                    src="http://commons.wikimedia.org/wiki/Special:FilePath/Lyndon_Rive_2015.jpg?width=300"
+                    class="img-responsive" alt="img"></a>
+
+                <div class="journal-txt">
+
+                  <h4><a href="blog-single.html">Lyndon Rive</a></h4>
+
+
+                </div>
+
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
+
+    </div>
+    <!-- End Journal Section -->
+
+
+
+
+    </div><!-- End Journal Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+      class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/typed.js/typed.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+  <?php
+   $latitude =-17;
+   $longitude = 30;
+    ?>
+
+  <script>
+    var map = L.map('map').setView([<?=$latitude?>, <?=$longitude?>], 13);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+    var marker = L.marker([<?=$latitude?>, <?=$longitude?>]).addTo(map);
+    marker.bindPopup("Elon Birth Here").openPopup();
+  </script>
+
+</body>
+
+</html>
