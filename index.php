15edcc37cbf8688fd55dd765526716b1bcca5329
Febri Zuladhari <febrizuladhari12345@gmail.com>
2022-12-26 23:15:38 +0700

    ketinggalan

--- index.php
+++ index.php
@@ -7,7 +7,6 @@ use EasyRdf\RdfNamespace;
     \EasyRdf\RdfNamespace::set('rdf', 'http://www.w3.org/1999/02/22-rdf-syntax-ns#');
     \EasyRdf\RdfNamespace::set('rdfs', 'http://www.w3.org/2000/01/rdf-schema#');
     \EasyRdf\RdfNamespace::set('owl', 'http://www.w3.org/2002/07/owl#');
-    \EasyRdf\RdfNamespace::set('dc', 'http://purl.org/dc/terms/');
     \EasyRdf\RdfNamespace::set('dbo', 'http://dbpedia.org/ontology/');
     \EasyRdf\RdfNamespace::set('dbp', 'http://dbpedia.org/property/');
     \EasyRdf\RdfNamespace::set('xsd', 'http://www.w3.org/2001/XMLSchema#');
